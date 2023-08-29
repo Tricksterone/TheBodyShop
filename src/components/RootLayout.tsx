56733a5596a560e519aabf9b5f3077dd127b8ccd
Elin Nyman <elin.nyman87@gmail.com>
2023-08-29 21:19:07 +0200

    Header and logo

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,10 +1,37 @@
+import { AppBar, Box, IconButton, Toolbar, styled } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const StyledAppBar = styled(AppBar)({
+  position: "static",
+  backgroundColor: "#A3D4DB",
+  color: "white",
+  padding: "1rem",
+});
+
+const LogoImage = styled("img")({
+  width: "50px",
+  height: "auto",
+});
 
 export default function RootLayout() {
   return (
     <div>
       <header>
-        <h1>Root Layout</h1>
+        <StyledAppBar>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <LogoImage src={"../../public/images/logo.png"} alt="Logo" />
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <IconButton color="inherit" aria-label="profile">
+                <AccountCircleIcon />
+              </IconButton>
+              <IconButton color="inherit" aria-label="cart">
+                <ShoppingCartIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </StyledAppBar>
       </header>
       <main>
         <Outlet />
