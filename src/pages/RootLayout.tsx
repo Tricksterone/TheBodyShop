5076de9c0fafcb085a91b9f949a2e66cde8e1194
Elin Nyman <elin.nyman87@gmail.com>
2023-08-29 10:35:04 +0200

    Outlet added

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 export default function RootLayout() {
   return (
     <div>
       <header>
         <h1>Root Layout</h1>
       </header>
+      <main>
+        <Outlet />
+      </main>
       <footer>
         <h2>Footer</h2>
       </footer>
