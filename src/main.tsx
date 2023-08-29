cdd16c3c3472779b688d91948d5bcb4842bb5e3b
Tricksterone <Johan.karlsson089@gmail.com>
2023-08-29 10:26:20 +0200

    Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import RootLayout from "./pages/RootLayout";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Confirmation from "./pages/Confirmation";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />}>
+      <Route path="products" element={<Products />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="checkout" element={<Checkout />} />
+      <Route path="confirmation" element={<Confirmation />} />
+      <Route path="*" element={<div>Not Found </div>} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/RootLayout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout() {
+  return (
+    <div>
+      <header>
+        <h1>Root Layout</h1>
+      </header>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </div>
+  );
+}
