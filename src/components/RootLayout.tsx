fb45ccfddc2cfa771da21409f272157db277c0a5
Elin Nyman <elin.nyman87@gmail.com>
2023-08-30 11:26:51 +0200

    <main> and <footer> styled

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,4 +1,11 @@
-import { AppBar, Box, IconButton, Toolbar, styled } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  styled,
+} from "@mui/material";
 import { Outlet } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -10,9 +17,27 @@ const StyledAppBar = styled(AppBar)({
   padding: "1rem",
 });
 
-const LogoImage = styled("img")({
+const StyledLogoImage = styled("img")({
   width: "50px",
   height: "auto",
+});
+
+const StyledMainContainer = styled(Container)({
+  backgroundColor: "#fafaf8",
+  display: "flex",
+  justifyContent: "space-evenly",
+});
+
+const StyledFooterContainer = styled(Container)({
+  position: "absolute",
+  bottom: "0",
+  width: "100%",
+  height: "2.5rem",
+  backgroundColor: "#fafaf8",
+});
+
+const StyledMainBox = styled(Box)({
+  display: "flex",
 });
 
 export default function RootLayout() {
@@ -21,7 +46,7 @@ export default function RootLayout() {
       <header>
         <StyledAppBar>
           <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-            <LogoImage src={"../../public/images/logo.png"} alt="Logo" />
+            <StyledLogoImage src={"../../public/images/logo.png"} alt="Logo" />
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <IconButton color="inherit" aria-label="profile">
                 <AccountCircleIcon />
@@ -34,10 +59,12 @@ export default function RootLayout() {
         </StyledAppBar>
       </header>
       <main>
-        <Outlet />
+        <StyledMainContainer>
+          <Outlet />
+        </StyledMainContainer>
       </main>
       <footer>
-        <h2>Footer</h2>
+        <StyledFooterContainer>FOOTER</StyledFooterContainer>
       </footer>
     </div>
   );
