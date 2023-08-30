af17818bf8e822df638eee036ae842f5e15010dc
Tricksterone <Johan.karlsson089@gmail.com>
2023-08-30 10:31:04 +0200

    added function for Alert onClick

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,29 @@
+import * as React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import { useState } from "react";
 
 export default function ProductCard() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowAlert(true);
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
-        height="270"
+        height="400"
         image="../images/heart.jpg"
         alt="Human Heart"
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quis
-          magni veritatis quos corporis natus, debitis enim temporibus unde
-          iusto, id aperiam veniam doloremque modi qui numquam laborum magnam.
-          Optio?
-        </Typography>
         <br />
         <Typography
           display="flex"
@@ -28,10 +31,17 @@ export default function ProductCard() {
           alignItems="center"
         >
           25000 kr
-          <Button size="medium" variant="contained">
+          <Button size="medium" variant="contained" onClick={handleButtonClick}>
             Add to cart
           </Button>
         </Typography>
+        <br />
+        {/* alert to show when clicking add to cart */}
+        {showAlert && (
+          <Alert data-cy="added-to-cart-toast" severity="success" color="info">
+            Product added to cart!
+          </Alert>
+        )}
       </CardContent>
       <CardActions disableSpacing></CardActions>
     </Card>
