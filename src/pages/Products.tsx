229040ab40794cad36752df24397ed93551b82b3
Elin Nyman <elin.nyman87@gmail.com>
2023-08-30 13:43:32 +0200

    Some styling for heroimage and the slogan

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -61,16 +61,45 @@ const StyledMainBox = styled(Box)({
   backgroundColor: "#fafaf8",
 });
 
-const StyledHeroImageBox = styled(Box)({
+const StyledHeroImageBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignContent: "center",
   width: "100%",
   height: "28rem",
-});
+  [theme.breakpoints.down("md")]: {
+    height: "20rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    height: "15rem",
+  },
+}));
 
 const StyledHeroImage = styled("img")({
   width: "100%",
   height: "100%",
   objectFit: "cover",
 });
+
+const StyledSlogan = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  color: "#377794",
+  right: "5%",
+  top: "45%",
+  fontSize: "3rem",
+  fontWeight: "bold",
+  textAlign: "right",
+  [theme.breakpoints.down("md")]: {
+    top: "40%",
+    fontSize: "2rem",
+    right: "1%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    top: "25%",
+    fontSize: "1.5rem",
+    transform: "translateY(-150%)",
+  },
+}));
 
 const StyledProductsContainer = styled(Container)({
   display: "flex",
@@ -84,9 +113,10 @@ export default function Products() {
       <StyledMainBox>
         <StyledHeroImageBox>
           <StyledHeroImage
-            src="../../public/images/hero-2.png"
+            src="../../public/images/hero125039.png"
             alt="Two smiling doctors in a hospital environment"
           ></StyledHeroImage>
+          <StyledSlogan>All our organs are organic</StyledSlogan>
         </StyledHeroImageBox>
         <StyledProductsContainer>
           <Grid container spacing={2}>
