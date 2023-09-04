87ed72fa10c1f24fa5f76c1d2e8d432abd243ece
Elin Nyman <elin.nyman87@gmail.com>
2023-09-04 09:37:19 +0200

    CartContext med LocalStorage hooks :)

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const lsItem = localStorage.getItem(key);
+    if (lsItem != null) return JSON.parse(lsItem);
+
+    if (typeof initialValue === "function") {
+      return (initialValue as () => T)();
+    } else {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
