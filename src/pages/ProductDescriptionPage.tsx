0cb9db79e4142197aab3d11f702dbf0adb67bf37
Elin Nyman <elin.nyman87@gmail.com>
2023-09-13 09:23:40 +0200

    Done

--- src/pages/ProductDescriptionPage.tsx
+++ src/pages/ProductDescriptionPage.tsx
@@ -1,82 +1,3 @@
-// import { Grid, Snackbar, SnackbarContent, styled } from "@mui/material";
-// import Typography from "@mui/material/Typography";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { products } from "../../data/index";
-// import QuantityButton from "../components/QuantityButton";
-// import { useCart } from "../context/CartContext";
-
-// export default function ProductDescription() {
-//   const {
-//     getItemQuantity,
-//     increaseCartQuantity,
-//     decreaseCartQuantity,
-//     removeFromCart,
-//   } = useCart();
-//   const { id } = useParams();
-
-//   let product = products.find((p) => p.id === id);
-
-//   const ResponsiveGridItem = styled(Grid)(({ theme }) => ({
-//     display: "flex",
-//     justifyContent: "flex-end", // Center on mobile screens
-//     alignItems: "center", // Center vertically on all screens
-//     [theme.breakpoints.down("md")]: {
-//       justifyContent: "flex-start",
-//     },
-//     [theme.breakpoints.down("sm")]: {
-//       justifyContent: "flex-start",
-//     },
-//   }));
-
-//   const ResponsiveImage = styled("img")(({ theme }) => ({
-//     display: "flex",
-//     maxWidth: "100%",
-//     height: "auto",
-//     [theme.breakpoints.down("md")]: {
-//       objectFit: "auto",
-//     },
-//     [theme.breakpoints.down("sm")]: {
-//       objectFit: "auto",
-//       maxWidth: "auto",
-//       maxHeight: "450px",
-//     },
-//   }));
-
-//   if (!product) {
-//     return <div>Product not found.</div>;
-//   }
-
-//   return (
-//     <>
-//       <Grid container spacing={2}>
-//         {/* Image */}
-//         <ResponsiveGridItem item xs={12} sm={6}>
-//           <ResponsiveImage src={product.image} alt={product.image} />
-//         </ResponsiveGridItem>
-
-//         {/* Content */}
-//         <Grid item xs={12} sm={6} marginTop={7}>
-//           <Typography data-cy="product-title" variant="h4">
-//             {product.title}
-//           </Typography>
-//           <Typography
-//             data-cy="product-description"
-//             variant="body1"
-//             sx={{ maxWidth: "400px" }}
-//           >
-//             {product.description}
-//           </Typography>
-//           <Typography data-cy="product-price" variant="h6">
-//             {product.price}$
-//           </Typography>
-//           <QuantityButton product={product} />
-//         </Grid>
-//       </Grid>
-//     </>
-//   );
-// }
-
 import { Box, Typography, styled } from "@mui/material";
 import { useParams } from "react-router-dom";
 import QuantityButton from "../components/QuantityButton";
@@ -105,14 +26,18 @@ const SecondBox = styled(Box)(({ theme }) => ({
     flexDirection: "column",
   },
 }));
-const Item1 = styled(Box)({
+const Item1 = styled(Box)(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
   // backgroundColor: "cyan",
   padding: "1rem",
-  height: "full",
   width: "100%",
-});
+  objectFit: "contain",
+  [theme.breakpoints.between("xs", "md")]: {
+    width: "95%",
+    height: "200px",
+  },
+}));
 const Image = styled("img")(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
@@ -180,7 +105,7 @@ export default function Test() {
   const { id } = useParams();
   const { products } = useProducts();
 
-  const product = products.find((p) => p.id === id);
+  let product = products.find((p) => p.id === id);
 
   return (
     <>
@@ -192,16 +117,24 @@ export default function Test() {
           <Item2>
             <TextBox>
               <StyledTitle>
-                <Typography sx={{ fontSize: "h4.fontSize" }}>
+                <Typography
+                  data-cy="product-title"
+                  sx={{ fontSize: "h4.fontSize" }}
+                >
                   {product?.title}
                 </Typography>
               </StyledTitle>
               <StyledDescription>
-                <Typography>{product?.description}</Typography>
+                <Typography data-cy="product-description">
+                  {product?.description}
+                </Typography>
               </StyledDescription>
             </TextBox>
             <PriceBox>
-              <Typography sx={{ fontWeight: "bold", fontSize: "18px" }}>
+              <Typography
+                data-cy="product-price"
+                sx={{ fontWeight: "bold", fontSize: "18px" }}
+              >
                 $ {product?.price}
               </Typography>
             </PriceBox>
